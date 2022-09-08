6244f98311d2b2a22bbcf72fe526eb7d51967866
Vince <vince@vcmedia.io>
2022-09-08 14:10:22 -0400

    Test

--- controllers/franchises.js
+++ controllers/franchises.js
@@ -23,7 +23,7 @@ exports.getFranchise = async (req, res, next) => {
     const franchise = await Franchise.findById(req.params.id)
 
     if(!franchise){
-      return res.status(400).json({ success: false})
+      return res.status(400).json({ success: true})
     }
 
     res.status(200).json({ success: true, data: franchise })
