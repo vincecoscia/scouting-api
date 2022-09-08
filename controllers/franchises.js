87f3146b8a0203b877d0d68c36d06d10a4da52ab
Vince <xvincentcosciax@gmail.com>
2022-09-08 15:14:05 -0400

    added asyncHandler

--- controllers/franchises.js
+++ controllers/franchises.js
@@ -1,88 +1,79 @@
-const ErrorResponse = require('../utils/errorResponse');
-const Franchise = require('../models/Franchise')
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const Franchise = require("../models/Franchise");
 
 // @desc    Get all Franchises
 // @route   Get /api/v1/franchises
 // @access  Private
-exports.getFranchises = async (req, res, next) => {
-  try {
-    const franchises = await Franchise.find();
+exports.getFranchises = asyncHandler(async (req, res, next) => {
+  const franchises = await Franchise.find();
 
-    res.status(200).json({
-      success: true, count: franchises.length, data: franchises
-    })
-  } catch (err) {
-    next(err)
-  }
-}
+  res.status(200).json({
+    success: true,
+    count: franchises.length,
+    data: franchises,
+  });
+});
 
 // @desc    Get single Franchise
 // @route   Get /api/v1/franchises/:id
 // @access  Private
-exports.getFranchise = async (req, res, next) => {
-  try {
-    const franchise = await Franchise.findById(req.params.id)
+exports.getFranchise = asyncHandler(async (req, res, next) => {
+  const franchise = await Franchise.findById(req.params.id);
 
-    if(!franchise){
-      return next(new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404))
-    }
-
-    res.status(200).json({ success: true, data: franchise })
-  } catch (err) {
-    next(err)
+  if (!franchise) {
+    return next(
+      new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404)
+    );
   }
-}
+
+  res.status(200).json({ success: true, data: franchise });
+
+  next(err);
+});
 
 // @desc    Create a Franchise
 // @route   Get /api/v1/franchises
 // @access  Private
-exports.createFranchise = async (req, res, next) => {
-  try {
-    const franchise = await Franchise.create(req.body)
+exports.createFranchise = asyncHandler(async (req, res, next) => {
+  const franchise = await Franchise.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: franchise
-    })
-  } catch (err) {
-    next(err)
-  }
+  res.status(201).json({
+    success: true,
+    data: franchise,
+  });
 
-}
+  next(err);
+});
 
 // @desc    Update single Franchise
 // @route   Get /api/v1/franchises/:id
 // @access  Private
-exports.updateFranchise = async (req, res, next) => {
-  try {
-    const franchise = await Franchise.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    if(!franchise){
-      return next(new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404))
-    }
-  
-    res.status(200).json({ success: true, data: franchise})
-  } catch (err) {
-    next(err)
+exports.updateFranchise = asyncHandler(async (req, res, next) => {
+  const franchise = await Franchise.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!franchise) {
+    return next(
+      new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404)
+    );
   }
 
-}
+  res.status(200).json({ success: true, data: franchise });
+});
 
 // @desc    Delete single Franchise
 // @route   Get /api/v1/franchises/:id
 // @access  Private
-exports.deleteFranchise = async (req, res, next) => {
-  try {
-    const franchise = await Franchise.findByIdAndDelete(req.params.id);
-    
-    if(!franchise){
-      return next(new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404))
-    }
-  
-    res.status(200).json({ success: true, data: {}})
-  } catch (err) {
-    next(err)
+exports.deleteFranchise = asyncHandler(async (req, res, next) => {
+  const franchise = await Franchise.findByIdAndDelete(req.params.id);
+
+  if (!franchise) {
+    return next(
+      new ErrorResponse(`Franchise not found with id of ${req.params.id}`, 404)
+    );
   }
-}
+
+  res.status(200).json({ success: true, data: {} });
+});
